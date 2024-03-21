1c6aa27a210b618849d669a6d51f8d48ec29b1a2
Benhin <benhinmwendwa7@gmail.com>
2024-03-21 14:55:49 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = `bob`
+var bestCustomer 
+const leastFavoriteCustomer = `John`
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+function setBestCustomer(){
+    return bestCustomer= `not bob`
+    }
+    function overwriteBestCustomer()
+    {
+      return  bestCustomer = `maybe bob`
+    }
+    function changeLeastFavoriteCustomer(){
+        return leastFavoriteCustomer = `ben`
+    }
